da2c160052c7a2678af21df390b04af935a64273
fuckbili <76082464+fuckbili@users.noreply.github.com>
2021-03-11 22:27:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,22 +7,42 @@ const net = require('./Config/net');
 
 app.get('/intl/gateway/v2/ogv/view/app/season', net.ip, net.auth, async (req, res, next) => {
 	data = await net.th_season(req.query)
+	res.set({
+		'Access-Control-Allow-Origin': 'https://www.bilibili.com',
+		'Access-Control-Allow-Credentials': 'true'
+	})
 	res.send(data)
 });
 app.get('/pgc/player/api/playurl', net.ip, net.auth, async (req, res, next) => {
 	data = await net.playurl(req.query)
+	res.set({
+		'Access-Control-Allow-Origin': 'https://www.bilibili.com',
+		'Access-Control-Allow-Credentials': 'true'
+	})
 	res.send(data)
 });
 app.get('/intl/gateway/v2/ogv/playurl', net.ip, net.auth, async (req, res, next) => {
 	data = await net.th_playurl(req.query)
+	res.set({
+		'Access-Control-Allow-Origin': 'https://www.bilibili.com',
+		'Access-Control-Allow-Credentials': 'true'
+	})
 	res.send(data)
 });
 app.get('/intl/gateway/v2/app/subtitle', net.ip, async (req, res, next) => {
 	data = await net.th_subtitle(req.query)
+	res.set({
+		'Access-Control-Allow-Origin': 'https://www.bilibili.com',
+		'Access-Control-Allow-Credentials': 'true'
+	})
 	res.send(data)
 });
 app.get('/intl/gateway/v2/app/search/type', net.ip, net.auth, async (req, res, next) => {
 	data = await net.th_search(req.query)
+	res.set({
+		'Access-Control-Allow-Origin': 'https://www.bilibili.com',
+		'Access-Control-Allow-Credentials': 'true'
+	})
 	res.send(data)
 });
 app.get('/pgc/player/web/playurl', net.ip, net.auth, async (req, res, next) => {
