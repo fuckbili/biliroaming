05a26e43b601fea572214058b523f37c4a549f1c
秋桜 <73423582+SakuraAkino@users.noreply.github.com>
2021-06-18 12:30:15 +0800

    Update th_season.js

--- Config/api/th_season.js
+++ Config/api/th_season.js
@@ -1,7 +1,7 @@
 const appkey = require('../config.js').bstar_a_ak
 
 const headers = {
-    'Host': 'api.global.bilibili.com',
+    'Host': 'app.biliintl.com',
     'APP-KEY': 'android',
     'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 11; IN2020 Build/RP1A.201005.001)',
 }
